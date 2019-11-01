f9babaf466baba77404d805180c288d438ce402f
lookung <uminto532@gmail.com>
2019-11-01 12:41:42 +0900

    いいね機能（まだ非同期通信はまだうまくいってない）

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,20 @@
 class Post < ApplicationRecord
 belongs_to :user
 has_many :comments
+has_many :likes, dependent: :destroy
+has_many :iine_users, through: :likes, source: :user
+
+  def iine?(user)
+    iine_users.include?(user)
+  end
+
+  def iine(user)
+    likes.create(user_id: user.id)
+  end
+
+  def uniine(user)
+    likes.find_by(user_id: user.id).destroy
+  end
+
 end
 
